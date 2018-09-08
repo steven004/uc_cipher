846ccd506b5458ba694b6f18df78eed19ee98fb1
zxm <zxm@lis-MacBook-Air.local>
2018-09-08 21:28:25 +0800

    Fix the print

--- test_some.py
+++ test_some.py
@@ -3,19 +3,18 @@ from ctypes import *
 
 
 def print_key(key, length=32):
-    print("key:")
     for i in xrange(length):
-        print("%x"%(key.value[i])) ,
+        print("%2x"%ord(key.raw[i])),
     print("\n")
 
 uc_cipher = cdll.LoadLibrary("/home/steven004/uc_cipher/uc_cipher.so")
 
 def test_fp(user_info):
     user_fp = create_string_buffer(32)
-    uc_cipher.UCES_user_fingerprint(user_fp, user_info, 80)
+    uc_cipher.UCES_user_fingerprint(user_fp, user_info, 60)
     # print_key(user_fp)
-    print(repr(user_info.raw))
-    print(repr(user_fp.raw), '\n')
+    print(repr(user_info))
+    print_key(user_fp)
 
 if __name__ == '__main__':
     user_info1 = create_string_buffer("The user's information, including many things", 80)
@@ -26,4 +25,3 @@ if __name__ == '__main__':
 
     user_info3 = create_string_buffer("Whatever the string is, let's do it", 80)
     test_fp(user_info3)
-    
